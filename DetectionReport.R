166228f4bc3515cc1d9661ce719a5c6264ad9cd0
j-sayers <55001144+j-sayers@users.noreply.github.com>
2022-05-11 11:29:51 -0400

    Update DetectionReport.R

--- DetectionReport.R
+++ DetectionReport.R
@@ -16,16 +16,20 @@
 # 1) List of stations
 # 2) A project's tags
 # 3) A custom query
-summarize_by <- 2
+summarize_by <- 1
 
 
 
 # provide a list of station(s) to summarize
 stations <- c(
-  11209 # Aylmer
+  # 11209, # Aylmer
   # 9912, # Walsingham
   # 11426, # Lemoine Point
   # 9913 # West Port Bruce
+  # 11578, # Russel Reid
+  # 10132, # Maple Leaf
+  # 11356 # Glass
+  11512 # Otter Lake
 )
 
 
