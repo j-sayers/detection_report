74b4b70811a31e442ccb015e041be66a2734b74a
j-sayers <55001144+j-sayers@users.noreply.github.com>
2022-10-19 14:28:08 -0400

    minor

--- DetectionReport_ProvincialParks.R
+++ DetectionReport_ProvincialParks.R
@@ -11,25 +11,40 @@
 
 
 
-# provide a list of station(s) to summarize
+# ## Provincial Parks station(s) to summarize
+# stations <- c(
+#   11246, # Bronte Creek Provincial Park	
+#   11418, # Komoka Provincial Park	        
+#   11445, # MacGregor Point Provincial Park	
+#   11541, # Point Farms Provincial Park   	
+#   11573, # Rondeau Provincial Park     	
+#   11587, # Sandbanks Provincial Park		
+#   11604,  # Short Hills Provincial Park
+#   10008, # Long Point PP
+#   12203 # Old Cut
+# )
+
+
+## OPG Stations
 stations <- c(
-  11246, # Bronte Creek Provincial Park	
-  11418, # Komoka Provincial Park	        
-  11445, # MacGregor Point Provincial Park	
-  11541, # Point Farms Provincial Park   	
-  11573, # Rondeau Provincial Park     	
-  11587, # Sandbanks Provincial Park		
-  11604,  # Short Hills Provincial Park
-  10008, # Long Point PP
-  12203 # Old Cut
+  10652, # Des Joachims
+  10005, # Saunders
+  11486, # Nanticoke
+  11532, # Pickering
+  10180, # Darlington
+  10214 # Lennox
 )
 
+
+# connect to db to get station names to use in file names
+source('C:/GitHub/motus_scripts/helper_functions.R')
 
 
 # directory for taghits file etc
 datadir <- 'C:/users/dethier/OneDrive/R/StationSummary/'
 # define output directory for the reports
-outdir <- r'(J:\.shortcut-targets-by-id\0B17GutSl-qqiWmhRZ0dydDM4aVk\Motus\Station Reports\Ontario Provincial Parks\)'
+# outdir <- r'(J:\.shortcut-targets-by-id\0B17GutSl-qqiWmhRZ0dydDM4aVk\Motus\Station Reports\Ontario Provincial Parks\)'
+outdir <- r'(J:\.shortcut-targets-by-id\0B17GutSl-qqiWmhRZ0dydDM4aVk\Motus\Station Reports\OPG\)'
 
 
 
